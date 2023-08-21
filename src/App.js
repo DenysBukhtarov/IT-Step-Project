a0a1f4061cb14a07385260c6f79165c126ece3c9
DENYS BUKHTAROV <denis180984@gmail.com>
2023-08-21 14:45:58 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavBar} from './components/NavBar';
+import {Banner} from './components/Banner';
+import {Skills} from './components/Skills';
+<<<<<<< Updated upstream
+import { ProjectCard } from './components/ProjectCard';
+import {Footer} from './components/Footer';
+=======
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+<<<<<<< Updated upstream
+      <ProjectCard />
+      <Skills />
+      <Footer />
+=======
+      <Skills />
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import contactImg from '../assets/img/contact-img.svg';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Contact = () => {
+  const formInitialDetails = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phones: '',
+    message: '',
+  };
+
+  const [formDetails, setFormDetails] = useState(formInialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
+  };
+  // continue
+};
